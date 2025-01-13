2e07ef3903d1decdec977f91ca8a68469e47d4b2
Gabriele898 <46900607+Gabriele898@users.noreply.github.com>
2025-01-13 22:57:48 +0100

    aggiornamenrto-001

---
+++ SupabaseClient.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://acanrjccdzyrarquivkb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYW5yamNjZHp5cmFycXVpdmtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNjc0NTQsImV4cCI6MjA1MTg0MzQ1NH0.ljZLA5asW_vsrdd7Kd2Zimkc5wnqhbAXu2EdQbMunhE';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export { supabase };
--- screens/CategoriaScreen.js
+++ screens/CategoriaScreen.js
@@ -7,6 +7,7 @@ const supabaseUrl = 'https://acanrjccdzyrarquivkb.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYW5yamNjZHp5cmFycXVpdmtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNjc0NTQsImV4cCI6MjA1MTg0MzQ1NH0.ljZLA5asW_vsrdd7Kd2Zimkc5wnqhbAXu2EdQbMunhE';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
+
 const CategoriaScreen = ({ route, navigation }) => {
   const { nome } = route.params;
   const [semilavorati, setSemilavorati] = useState([]);
